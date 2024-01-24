436ade40de51894fdc6bf902f11ab8f9fd487497
prapatn <prapat.werapan@gmail.com>
2024-01-24 11:15:43 +0700

    writeToCSV

--- player/services/services.go
+++ player/services/services.go
@@ -1,10 +1,14 @@
 package services
 
 import (
+	"encoding/csv"
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
@@ -21,16 +25,29 @@ func NewService() Services {
 	return service{}
 }
 
+var matchNumber int
+
 func (s service) InsertLog() error {
+	//CSV
+	matchNumber = int(time.Now().UnixNano())
+	fmt.Printf("Match %v started!\n", matchNumber)
+	csvFile, err := os.Create(fmt.Sprintf("csv-log/match_%v_log.csv", matchNumber))
+	if err != nil {
+		log.Println(err)
+		return errors.New(fmt.Sprintf("Create file match_%v_log.csv fail", matchNumber))
+	}
+
+	csvwriter := csv.NewWriter(csvFile)
+	defer csvwriter.Flush()
+
 	chA := make(chan int)
 	chB := make(chan int)
 	errs := make(chan error)
 
-	go player("A", chA, chB, errs)
-	go player("B", chB, chA, errs)
+	go player("A", chA, chB, errs, csvwriter)
+	go player("B", chB, chA, errs, csvwriter)
 
 	// Start Player A
-	fmt.Println("New match started!")
 	chA <- 0
 
 	if err, ok := <-errs; ok {
@@ -40,8 +57,8 @@ func (s service) InsertLog() error {
 	return nil
 }
 
-func player(name string, receive chan int, send chan int, errs chan error) {
-	for {
+func player(name string, receive chan int, send chan int, errs chan error, csvwriter *csv.Writer) {
+	for turn := 1; ; turn++ {
 		ballPowerReceive := <-receive
 		if ballPowerReceive < 0 {
 			close(receive)
@@ -54,12 +71,23 @@ func player(name string, receive chan int, send chan int, errs chan error) {
 		ballPowerSend := rand.Intn(100) + 1
 		if ballPowerReceive > ballPowerSend {
 			fmt.Printf("Player %v loses (Power : %v)\n", name, ballPowerSend)
+			err := writeToCSV(name, turn, ballPowerSend, csvwriter)
+			if err != nil {
+				errs <- err
+			}
 			send <- -1
 			break
 		}
 
 		//Modified power
 		err := tablePing(&ballPowerSend)
+		if err != nil {
+			errs <- err
+			send <- -1
+			break
+		}
+
+		err = writeToCSV(name, turn, ballPowerSend, csvwriter)
 		if err != nil {
 			errs <- err
 			send <- -1
@@ -86,5 +114,16 @@ func tablePing(ballPower *int) error {
 	}
 	*ballPower, _ = strconv.Atoi(string(data))
 
+	return nil
+}
+
+func writeToCSV(playerName string, turn int, ballPower int, csvwriter *csv.Writer) error {
+	now := time.Now().Format("2006-01-02 15:04:05")
+	row := []string{strconv.Itoa(matchNumber), playerName, strconv.Itoa(turn), strconv.Itoa(ballPower), now}
+	err := csvwriter.Write(row)
+	if err != nil {
+		return err
+	}
+	csvwriter.Flush()
 	return nil
 }
